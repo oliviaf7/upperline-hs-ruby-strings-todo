347c74e59e5a62212fb32d0110fe49b59fee1206
Olivia Friedberg <olliefhome@gmail.com>
2017-06-13 14:30:44 +0000

    finished my invitation

---
+++ invitation.rb
@@ -0,0 +1,7 @@
+puts "what is the guest name"
+guest_name = gets.chomp 
+puts "what is the party name"
+party_name = gets.chomp 
+puts "Dear #{guest_name},"
+puts "Please come to my #{party_name}"
+puts "#My {party_name} is on June 20th"
